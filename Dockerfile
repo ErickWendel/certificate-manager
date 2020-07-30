47c8b163ca65218c69f4ec081a01f8bc9adaa08b
Erick Wendel <erick.workspace@gmail.com>
2020-07-30 20:55:59 -0300

    feat(): add worker threads approach
    Signed-off-by: Erick Wendel <erick.workspace@gmail.com>

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,10 @@
-FROM node:14-alpine as build
+FROM node:12-alpine as build
 
 WORKDIR /src/
 
 COPY package.json package-lock.json /src/
 
-RUN  npm ci --silent
+RUN  npm i --silent
 
 COPY . .
 
@@ -16,4 +16,4 @@ COPY --from=build /src/node_modules node_modules
 
 USER node
 
-CMD npm start
+CMD npm run dev
---
+++ src/index.js
@@ -0,0 +1,17 @@
+
+const moduleName = `${__dirname}/worker.js`
+const { Worker } = require('worker_threads')
+
+const data = require('../resources/data.json')
+async function run() {
+    let count = 0
+    for (const item of data) {
+        item.name = `${item.name}${count++}`
+        const worker = new Worker(moduleName)
+        worker.on('message', msg => console.log(msg))
+        worker.on('error', console.error)
+        worker.postMessage(item)
+    }
+}
+
+run()
